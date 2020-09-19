b947116f86f6c9c017b98bcb1037c12d5c40b893
Lee JE <jes7077@naver.com>
2020-09-19 14:23:15 +0900

    [#1] feat: 프로젝트 생성

---
+++ src/main/java/com/festa/FestaApplication.java
@@ -0,0 +1,13 @@
+package com.festa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FestaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FestaApplication.class, args);
+    }
+
+}
