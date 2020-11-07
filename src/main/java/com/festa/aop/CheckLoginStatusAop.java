573b5334f6c204667c128a32af66bd04ae88497e
Lee JE <jes7077@naver.com>
2020-11-08 00:50:43 +0900

    [#31] refactor: swtich-case default 제거

--- src/main/java/com/festa/aop/CheckLoginStatusAop.java
+++ src/main/java/com/festa/aop/CheckLoginStatusAop.java
@@ -44,9 +44,6 @@ public class CheckLoginStatusAop {
             case USER :
                 userLoginStatus();
                 break;
-
-            default:
-                throw new NoSuchElementException();
         }
     }
 
