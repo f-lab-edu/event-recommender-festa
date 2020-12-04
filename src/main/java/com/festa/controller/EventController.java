291c4dac61b19d4200d427995211baad4cf89a19
YoonJeong <53729311+Yo0oN@users.noreply.github.com>
2020-12-04 17:09:27 +0900

    feat:사용자(회원+주최자) 이벤트 상세보기 기능
    EventController.java의 getInfoOfEvent 메서드의 리턴값을 Optional<EventDTO>로 수정
    
    [#36] 사용자(회원+주최자) 이벤트 상세보기 기능 추가

--- src/main/java/com/festa/controller/EventController.java
+++ src/main/java/com/festa/controller/EventController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/events")
@@ -22,13 +23,15 @@ public class EventController {
 
     /**
      * 이벤트 상세보기
+     *
+     * Optional : java8에서 도입, null이 될 수도 있는 객체를 감싸 예외가 발생하는것을 방지하거나, 해당 변수가 null일 수 있다는 가능성을 표현해준다.
+     *          ofNullable 메서드는 value가 null이더라도 비어있는 Optional을 반환해준다.
      * @param eventNo
-     * @return
+     * @return Optional<EventDTO>
      */
     @CheckLoginStatus(auth = UserLevel.USER)
     @GetMapping("/{eventNo}")
-    public ResponseEntity<EventDTO> getInfoOfEvent(@PathVariable int eventNo) {
-        EventDTO infoOfEvent = eventService.getInfoOfEvent(eventNo);
-        return ResponseEntity.ok(infoOfEvent);
+    public Optional<EventDTO> getInfoOfEvent(@PathVariable int eventNo) {
+        return Optional.ofNullable(eventService.getInfoOfEvent(eventNo));
     }
 }
