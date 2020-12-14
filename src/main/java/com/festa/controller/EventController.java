2ddadeadec885d4ff3467872a831f921e828f19f
YoonJeong <53729311+Yo0oN@users.noreply.github.com>
2020-12-15 00:05:17 +0900

    feat:사용자(회원+주최자) 이벤트 상세보기 기능
    리턴값을 EventDTO로 수정, 이벤트 정보가 없을 경우 IllegalArgumentException 발생
    
    [#36]feat:사용자(회원+주최자) 이벤트 상세보기 기능

--- src/main/java/com/festa/controller/EventController.java
+++ src/main/java/com/festa/controller/EventController.java
@@ -5,14 +5,10 @@ import com.festa.common.UserLevel;
 import com.festa.dto.EventDTO;
 import com.festa.service.EventService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
-import java.util.Optional;
 
 @RestController
 @RequestMapping("/events")
@@ -24,14 +20,16 @@ public class EventController {
     /**
      * 이벤트 상세보기
      *
-     * Optional : java8에서 도입, null이 될 수도 있는 객체를 감싸 예외가 발생하는것을 방지하거나, 해당 변수가 null일 수 있다는 가능성을 표현해준다.
-     *          ofNullable 메서드는 value가 null이더라도 비어있는 Optional을 반환해준다.
      * @param eventNo
-     * @return Optional<EventDTO>
+     * @return EventDTO
      */
     @CheckLoginStatus(auth = UserLevel.USER)
     @GetMapping("/{eventNo}")
-    public Optional<EventDTO> getInfoOfEvent(@PathVariable int eventNo) {
-        return Optional.ofNullable(eventService.getInfoOfEvent(eventNo));
+    public EventDTO getInfoOfEvent(@PathVariable int eventNo) {
+        EventDTO infoOfEvent = eventService.getInfoOfEvent(eventNo);
+        if (infoOfEvent == null) {
+            throw new IllegalArgumentException("존재하지 않는 이벤트입니다.");
+        }
+        return infoOfEvent;
     }
 }
