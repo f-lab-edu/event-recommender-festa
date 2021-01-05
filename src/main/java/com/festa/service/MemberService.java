ded6abb7458d969a162435e74a0029351d1a1286
Lee JE <jes7077@naver.com>
2021-01-05 23:39:49 +0900

    [#80] fix: UPDATE SQL AND -> 콤마로 변경, userName 오타 수정

--- src/main/java/com/festa/service/MemberService.java
+++ src/main/java/com/festa/service/MemberService.java
@@ -2,13 +2,14 @@ package com.festa.service;
 
 import com.festa.dao.MemberDAO;
 import com.festa.dto.MemberDTO;
-import com.festa.model.MemberLogin;
 import com.festa.model.MemberInfo;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@Log4j2
 @RequiredArgsConstructor
 public class MemberService {
 
