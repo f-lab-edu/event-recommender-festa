30f7e71e94cc9207ec2a90293400196131abfa24
Lee JE <jes7077@naver.com>
2021-01-10 01:10:21 +0900

    [#86] test: 이메일, 전화번호, 비밀번호,아이디공백/정규표현식 테스트케이스 추가

--- src/test/java/com/festa/MemberServiceTests.java
+++ src/test/java/com/festa/MemberServiceTests.java
@@ -84,6 +84,158 @@ class MemberServiceTests {
         assertNotEquals(memberInfo.getPassword(), "", "비밀번호를 입력해주세요.");
     }
 
+    @DisplayName("이름 미입력시 회원가입 불가")
+    @Test
+    public void signUpWithoutUserNameTest() {
+        MemberDTO memberInfo = MemberDTO.builder()
+                .userId("jes7077")
+                .password("test123#")
+                .userName("")
+                .email("aaa@aaa.com")
+                .phoneNo("01033334444")
+                .userLevel(UserLevel.valueOf("USER"))
+                .cityName("서울")
+                .districtName("종로구")
+                .streetCode("1")
+                .streetName("종로")
+                .build();
+
+        assertNotEquals(memberInfo.getUserName(), "", "이름을 입력해주세요.");
+    }
+
+    @DisplayName("이메일 미입력시 회원가입 불가")
+    @Test
+    public void signUpWithoutEmailTest() {
+        MemberDTO memberInfo = MemberDTO.builder()
+                .userId("jes7077")
+                .password("test123#")
+                .userName("제인")
+                .email("")
+                .phoneNo("01033334444")
+                .userLevel(UserLevel.valueOf("USER"))
+                .cityName("서울")
+                .districtName("종로구")
+                .streetCode("1")
+                .streetName("종로")
+                .build();
+
+        assertNotEquals(memberInfo.getEmail(), "", "이메일을 입력해주세요.");
+    }
+
+    @DisplayName("전화번호 미입력시 회원가입 불가")
+    @Test
+    public void signUpWithoutPhoneTest() {
+        MemberDTO memberInfo = MemberDTO.builder()
+                .userId("jes7077")
+                .password("test123#")
+                .userName("제인")
+                .email("aaa@aaa.com")
+                .phoneNo("01033334444")
+                .userLevel(UserLevel.valueOf("USER"))
+                .cityName("서울")
+                .districtName("종로구")
+                .streetCode("1")
+                .streetName("종로")
+                .build();
+
+        assertNotEquals(memberInfo.getPhoneNo(), "", "전화번호를 입력해주세요.");
+    }
+
+    @DisplayName("전화번호 -(하이픈) 입력시 회원가입 불가")
+    @Test
+    public void signUpWithoutPhoneRegexTest() {
+        MemberDTO memberInfo = MemberDTO.builder()
+                .userId("jes7077")
+                .password("test123#")
+                .userName("제인")
+                .email("aaa@aaa.com")
+                .phoneNo("010-3333-4444")
+                .userLevel(UserLevel.valueOf("USER"))
+                .cityName("서울")
+                .districtName("종로구")
+                .streetCode("1")
+                .streetName("종로")
+                .build();
+
+        assertTrue(memberInfo.getPhoneNo().matches("(^02.{0}|^01.{1}|[0-9]{3})([0-9]{4})([0-9]{4})"));
+    }
+
+    @DisplayName("비밀번호 특수기호 미포함 회원가입 불가")
+    @Test
+    public void signUpPasswordWithoutSpecialSymbolTest() {
+        MemberDTO memberInfo = MemberDTO.builder()
+                .userId("jes7077")
+                .password("test12")
+                .userName("testUser")
+                .email("aaa@aaa.com")
+                .phoneNo("01033334444")
+                .userLevel(UserLevel.valueOf("USER"))
+                .cityName("서울")
+                .districtName("종로구")
+                .streetCode("1")
+                .streetName("종로")
+                .build();
+
+        assertTrue(memberInfo.getPassword().matches("(?=.*[0-9])(?=.*[a-zA-Z])(?=.*\\W)(?=\\S+$).{5,10}"));
+    }
+
+    @DisplayName("비밀번호 숫자 미포함 회원가입 불가")
+    @Test
+    public void signUpPasswordWithoutNumberTest() {
+        MemberDTO memberInfo = MemberDTO.builder()
+                .userId("jes7077")
+                .password("test#")
+                .userName("testUser")
+                .email("aaa@aaa.com")
+                .phoneNo("01033334444")
+                .userLevel(UserLevel.valueOf("USER"))
+                .cityName("서울")
+                .districtName("종로구")
+                .streetCode("1")
+                .streetName("종로")
+                .build();
+
+        assertTrue(memberInfo.getPassword().matches("(?=.*[0-9])(?=.*[a-zA-Z])(?=.*\\W)(?=\\S+$).{5,10}"));
+    }
+
+    @DisplayName("비밀번호 5자 이하일 경우 회원가입 불가")
+    @Test
+    public void signUpPasswordBelowFiveCharTest() {
+        MemberDTO memberInfo = MemberDTO.builder()
+                .userId("jes7077")
+                .password("t12#")
+                .userName("testUser")
+                .email("aaa@aaa.com")
+                .phoneNo("01033334444")
+                .userLevel(UserLevel.valueOf("USER"))
+                .cityName("서울")
+                .districtName("종로구")
+                .streetCode("1")
+                .streetName("종로")
+                .build();
+
+        assertTrue(memberInfo.getPassword().matches("(?=.*[0-9])(?=.*[a-zA-Z])(?=.*\\W)(?=\\S+$).{5,10}"));
+    }
+
+    @DisplayName("비밀번호 10자 이상일 경우 회원가입 불가")
+    @Test
+    public void signUpPasswordOverTenCharTest() {
+        MemberDTO memberInfo = MemberDTO.builder()
+                .userId("jes7077")
+                .password("test123123123###")
+                .userName("testUser")
+                .email("aaa@aaa.com")
+                .phoneNo("01033334444")
+                .userLevel(UserLevel.valueOf("USER"))
+                .cityName("서울")
+                .districtName("종로구")
+                .streetCode("1")
+                .streetName("종로")
+                .build();
+
+        assertTrue(memberInfo.getPassword().matches("(?=.*[0-9])(?=.*[a-zA-Z])(?=.*\\W)(?=\\S+$).{5,10}"));
+    }
+
     @DisplayName("회원 로그인 성공")
     @Test
     public void loginTest() {
@@ -114,5 +266,13 @@ class MemberServiceTests {
         MemberLogin memberLogin = new MemberLogin(5, null, "test123#");
 
         assertNotNull(memberLogin.getUserId());
+    }
+
+    @DisplayName("아이디가 공백일 경우 로그인 실패")
+    @Test
+    public void loginUserIdBlankTest() {
+        MemberLogin memberLogin = new MemberLogin(5, " ", "test123#");
+
+        assertNotEquals(memberLogin.getUserId(), " ", "아이디를 입력해주세요.");
     }
 }
