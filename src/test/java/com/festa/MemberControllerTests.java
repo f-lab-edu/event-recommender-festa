2527b6b2edaa099a5b0db166d143f8ed92959121
Lee JE <jes7077@naver.com>
2021-02-06 00:43:20 +0900

    [#99] test: 탈퇴회원, 로그인 테스트케이스 추가

--- src/test/java/com/festa/MemberControllerTests.java
+++ src/test/java/com/festa/MemberControllerTests.java
@@ -4,6 +4,7 @@ import com.festa.common.UserLevel;
 import com.festa.common.commonService.LoginService;
 import com.festa.controller.MemberController;
 import com.festa.dto.MemberDTO;
+import com.festa.model.MemberLogin;
 import com.festa.service.MemberService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -14,13 +15,13 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.ResultActions;
 
-import static org.hamcrest.Matchers.is;
-import static org.mockito.BDDMockito.given;
-import static org.mockito.Mockito.when;
+import static org.hamcrest.Matchers.*;
+import static org.mockito.BDDMockito.then;
+import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static com.festa.common.ResponseEntityConstants.RESPONSE_ENTITY_OK;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @ExtendWith(SpringExtension.class)
@@ -56,11 +57,51 @@ class MemberControllerTests {
     @DisplayName("존재하는 사용자정보를 조회하는 경우 200 상태코드를 리턴한다.")
     @Test
     public void getUserTest() throws Exception {
-        given(memberService.getUser(1)).willReturn(existedMemberInfo());
+        when(memberService.getUser(1)).thenReturn(existedMemberInfo());
 
-        mockMvc.perform(get("/members/{userNo}", "{userNo}")
-                .param("userNo", "1").contentType(MediaType.APPLICATION_JSON))
+        this.mockMvc.perform(get("/members/{userNo}", existedMemberInfo().getUserNo())
+                .param("userNo", "1"))
+                .andExpect(status().isOk());
+
+    }
+
+    @DisplayName("삭제된 사용자정보를 조회하는 경우 200 상태코드와 함께 빈 body를 리턴한다.")
+    @Test
+    public void getDeletedUserTest() throws Exception {
+        when(memberService.getUser(2)).thenReturn(null);
+
+        this.mockMvc.perform(get("/members/{userNo}", "{userNo}")
+                .param("userNo", "2"))
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$[0].userNo", is(existedMemberInfo().getUserNo())));
+                .andExpect(content().string(containsString("")));
+
+    }
+
+    @DisplayName("탈퇴하지 않은 사용자라면 service layer에서 예외가 발생하지 않으므로 200 상태코드를 리턴한다.")
+    @Test
+    public void isDeletedIdTest() throws Exception {
+        doNothing().when(memberService).isUserIdExist("jes7077", "123##test");
+
+        this.mockMvc.perform(get("/members/{userId}/delete", "{userId}")
+                .param("userId", "jes7077")
+                .param("password", "123##test"))
+                .andDo(print())
+                .andExpect(status().isOk());
+    }
+
+    @DisplayName("존재하는 회원이 로그인 요청을 하면 정상적으로 로그인한다.")
+    @Test
+    public void loginTest() throws Exception {
+        MemberLogin loginInfo = new MemberLogin(1L, "rbdl879", "test123##");
+
+        doNothing().when(memberService).isUserIdExist("rbdl879", "test123##");
+
+        this.mockMvc.perform(post("/members/login")
+                                .accept(MediaType.APPLICATION_JSON)
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content("{\"userNo\":\"1\",\"userId\":\"rbdl879\",\"password\":\"test123##\"}"))
+                                .andExpect(status().isOk());
+
+        then(loginService).should().setUserNo(loginInfo.getUserNo());
     }
 }
