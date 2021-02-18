a40f280bd4a9c2edac264ca267f20a560096455a
Lee JE <jes7077@naver.com>
2021-02-19 00:27:45 +0900

    [#61] feat, refactor: SimpleDateFormat -> joda-time으로 수정 및 의존성 추가

--- src/main/java/com/festa/common/util/ConvertData.java
+++ src/main/java/com/festa/common/util/ConvertData.java
@@ -1,15 +1,11 @@
 package com.festa.common.util;
 
-import java.text.SimpleDateFormat;
-import java.util.Date;
+import org.joda.time.LocalDate;
 
 public class ConvertData {
 
-    private static final SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
-
-
-    public static String getTodayDate() {
-        return dateFormat.format(new Date());
+    public static LocalDate getTodayDate() {
+        return LocalDate.now();
     }
 
     public static String longToString(long target) {
