58e305ee3c26142f4084bb59c75ae00cd18c7a7d
YoonJeong <53729311+Yo0oN@users.noreply.github.com>
2021-02-26 06:15:37 +0900

    feat: EventServiceTest 작성 중
    이벤트 목록 조회, 이벤트 번호 삭제를 제외한 정상적인 테스트 완료
    
    [#91] EventService 단위테스트

--- src/test/java/com/festa/service/EventServiceTest.java
+++ src/test/java/com/festa/service/EventServiceTest.java
@@ -3,6 +3,7 @@ package com.festa.service;
 import com.festa.dao.EventDAO;
 import com.festa.dto.EventDTO;
 import com.festa.model.PageInfo;
+import com.festa.model.Participants;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -17,7 +18,7 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(SpringExtension.class)
 class EventServiceTest {
@@ -54,7 +55,7 @@ class EventServiceTest {
 
     @Test
     @DisplayName("정상적인 이벤트 목록 조회")
-    void getEventsList() {
+    void getEventsListTest() {
         // given 이벤트 목록 생성
         List<EventDTO> eventsList = new ArrayList<>();
         eventsList.add(generateEvent(1));
@@ -62,7 +63,7 @@ class EventServiceTest {
 
         PageInfo pageInfo = new PageInfo(5, 5);
 
-        when(eventDAO.getListOfEvents(Mockito.any(PageInfo.class), Mockito.anyInt())).thenReturn(eventsList);
+        when(eventDAO.getListOfEvents(any(PageInfo.class), anyInt())).thenReturn(eventsList);
 
         // when
         List<EventDTO> resultEventList = eventService.getListOfEvents(pageInfo, 5);
@@ -73,11 +74,11 @@ class EventServiceTest {
 
     @Test
     @DisplayName("정상적인 이벤트 상세 조회")
-    void getInfoOfEvents() {
+    void getInfoOfEventsTest() {
         // given 이벤트 생성
         EventDTO event = generateEvent(555);
 
-        when(eventDAO.getInfoOfEvent(Mockito.anyLong())).thenReturn(event);
+        when(eventDAO.getInfoOfEvent(anyLong())).thenReturn(event);
 
         // when
         EventDTO resultEvent = eventService.getInfoOfEvent(555);
@@ -86,15 +87,192 @@ class EventServiceTest {
         assertEquals(event, resultEvent);
     }
 
-    @Test()
-    @DisplayName("비정상적인 이벤트 상세 조회")
-    void getInfoOfNullEvents() {
+    @Test
+    @DisplayName("정상적인 이벤트 등록")
+    void registerEventsTest() {
+        // given
         EventDTO event = generateEvent(555);
+        EventDTO eventInfo = event.toEntityForInfo();
+        EventDTO eventAddress =  EventDTO.builder()
+                .eventNo(eventInfo.getEventNo())
+                .cityName(event.getCityName())
+                .districtName(event.getDistrictName())
+                .streetCode(event.getStreetCode())
+                .streetName(event.getStreetName())
+                .detail(event.getDetail())
+                .build();
 
-        when(eventDAO.getInfoOfEvent(Mockito.anyLong())).thenReturn(event);
+        doNothing().when(eventDAO).registerEvents(eventInfo);
 
-        assertThrows(NullPointerException.class, () -> {
-            eventService.getInfoOfEvent(Mockito.isNull());
+        doNothing().when(eventDAO).registerEventsAddress(eventAddress);
+
+        // when
+        eventService.registerEvents(event, 5);
+
+        // then
+        verify(eventDAO).registerEvents(eventInfo);
+        verify(eventDAO).registerEventsAddress(eventAddress);
+    }
+
+    @Test
+    @DisplayName("정상적인 이벤트 수정")
+    void modifyEventsInfoTest() {
+        // given
+        EventDTO event = generateEvent(555);
+        EventDTO eventInfo = event.toEntityForInfo();
+
+        doNothing().when(eventDAO).modifyEventsInfo(eventInfo);
+
+        doNothing().when(eventDAO).modifyEventsAddress(event.toEntityForEventAddress());
+
+        // when
+        eventService.modifyEventsInfo(event);
+
+        // then
+        verify(eventDAO).modifyEventsInfo(eventInfo);
+        verify(eventDAO).modifyEventsAddress(event.toEntityForEventAddress());
+
+    }
+
+    @Test
+    @DisplayName("이벤트 존재 여부 확인 - 존재")
+    void isEventExistsTrueTest() {
+        // given
+        when(eventDAO.isEventExists("즐거운 컴퓨터 세계", "2021-03-05")).thenReturn(true);
+
+        // when
+        boolean result = eventService.isEventExists("즐거운 컴퓨터 세계", "2021-03-05");
+
+        //then
+        assertTrue(result);
+    }
+
+    @Test
+    @DisplayName("이벤트 존재 여부 확인 - 존재하지 않음")
+    void isEventExistsFalseTest() {
+        // given
+        when(eventDAO.isEventExists("즐거운 어린이날", "2021-05-05")).thenReturn(false);
+
+        // when
+        boolean result = eventService.isEventExists("즐거운 컴퓨터 세계", "2021-03-05");
+
+        //then
+        assertFalse(result);
+    }
+
+    @Test
+    @DisplayName("정상적인 이벤트 신청")
+    void applyForEventsTest() {
+        // given
+        EventDTO participantsInfo = EventDTO.builder()
+                .noOfParticipants(30)
+                .participantLimit(200)
+                .build();
+
+        Participants participants = Participants.builder()
+                .userNo(35)
+                .eventNo(555)
+                .cityName("서울")
+                .districtName("강남구")
+                .streetCode("45321")
+                .streetName("신사동")
+                .detail("32-12")
+                .build();
+
+        when(eventDAO.checkNoOfParticipants(participants.getEventNo())).thenReturn(participantsInfo);
+
+        doNothing().when(eventDAO).applyForEvents(participants);
+
+        doNothing().when(eventDAO).insertParticipantAddress(participants.toEntityForAddress());
+
+        doNothing().when(eventDAO).increaseParticipants(participants.getEventNo());
+
+        // when
+        eventService.applyForEvents(participants);
+
+        // then
+        verify(eventDAO).applyForEvents(participants);
+        verify(eventDAO).insertParticipantAddress(participants.toEntityForAddress());
+        verify(eventDAO).increaseParticipants(participants.getEventNo());
+    }
+
+    @Test()
+    @DisplayName("마감된 이벤트 신청")
+    void applyForFullEventsTest() {
+        // given
+        EventDTO eventParticipants = EventDTO.builder()
+                .noOfParticipants(200)
+                .participantLimit(200)
+                .build();
+
+        Participants participants = Participants.builder()
+                .userNo(35)
+                .eventNo(555)
+                .cityName("서울")
+                .districtName("강남구")
+                .streetCode("45321")
+                .streetName("신사동")
+                .detail("32-12")
+                .build();
+
+        // when
+        when(eventDAO.checkNoOfParticipants(anyLong())).thenReturn(eventParticipants);
+
+        assertThrows(IllegalStateException.class, () -> {
+            eventService.applyForEvents(participants);
+        });
+    }
+
+    @Test
+    @DisplayName("이벤트 신청 취소")
+    void cancelEventTest() {
+        // given
+        Participants participants = Participants.builder()
+                .userNo(35)
+                .eventNo(555)
+                .cityName("서울")
+                .districtName("강남구")
+                .streetCode("45321")
+                .streetName("신사동")
+                .detail("32-12")
+                .build();
+
+        doNothing().when(eventDAO).cancelEvent(participants.getUserNo());
+
+        when(eventDAO.isParticipated(participants.getUserNo())).thenReturn(true);
+
+        doNothing().when(eventDAO).reduceParticipants(participants.getEventNo());
+
+        // when
+        eventService.cancelEvent(participants);
+
+        // then
+        verify(eventDAO).cancelEvent(participants.getUserNo());
+        verify(eventDAO).reduceParticipants(participants.getEventNo());
+    }
+
+    @Test
+    @DisplayName("이벤트 신청 취소 실페 - 접수한 이벤트가 아님")
+    void cancelNoAppliedEventTest() {
+        // given
+        Participants participants = Participants.builder()
+                .userNo(35)
+                .eventNo(555)
+                .cityName("서울")
+                .districtName("강남구")
+                .streetCode("45321")
+                .streetName("신사동")
+                .detail("32-12")
+                .build();
+
+        doNothing().when(eventDAO).cancelEvent(participants.getUserNo());
+
+        when(eventDAO.isParticipated(participants.getUserNo())).thenReturn(false);
+
+        // when
+        assertThrows(IllegalStateException.class, () -> {
+            eventService.cancelEvent(participants);
+            verify(eventDAO).cancelEvent(participants.getUserNo());
         });
     }
 
