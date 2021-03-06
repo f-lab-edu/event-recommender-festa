db8e8d8d4989fcfd1998c8cac9619b105335e353
Lee JE <jes7077@naver.com>
2021-03-06 23:26:23 +0900

    [#103] feat: master/slave 분기처리를 위한 RoutingDataSource 클래스작성

---
+++ src/main/java/com/festa/common/RoutingDataSource.java
@@ -0,0 +1,16 @@
+package com.festa.common;
+
+import org.springframework.jdbc.datasource.lookup.AbstractRoutingDataSource;
+import org.springframework.transaction.support.TransactionSynchronizationManager;
+
+/*
+    Transactional이 readOnly 로 설정되어 있다면 Slave에서 읽어오도록 설정을 하기 위한 클래스
+ */
+public class RoutingDataSource extends AbstractRoutingDataSource {
+
+    @Override
+    protected Object determineCurrentLookupKey() {
+        return TransactionSynchronizationManager.isCurrentTransactionReadOnly()
+                ? "slave" : "master";
+    }
+}
