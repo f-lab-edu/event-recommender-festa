7add94f7fd9e05ab894b38d7652785a902ff56cd
Jane096 <jes7077@naver.com>
2021-05-05 01:20:47 +0900

    [#125] feat: AsyncConfig 클래스 생성

---
+++ src/main/java/com/festa/config/AsyncConfig.java
@@ -0,0 +1,31 @@
+package com.festa.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.AsyncConfigurer;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.ThreadPoolExecutor;
+
+/*
+    비동기 서비스를 위한 @Async 기능 활성화와 Thread Pool 설정을 위한 클래스
+ */
+
+@EnableAsync
+@Configuration
+public class AsyncConfig implements AsyncConfigurer {
+
+    @Override
+    public Executor getAsyncExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setThreadNamePrefix("festa-async-");
+        executor.setCorePoolSize(5);
+        executor.setMaxPoolSize(10);
+        executor.setQueueCapacity(100);
+        executor.setRejectedExecutionHandler(new ThreadPoolExecutor.CallerRunsPolicy());
+        executor.initialize();
+
+        return executor;
+    }
+}
